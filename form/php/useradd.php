1c2fc3d2c4e312882007e5df8788a1f7e5e63f26
fenasot <79359861+fenasot@users.noreply.github.com>
2023-04-26 08:06:15 +0800

    add creatacc.php

---
+++ form/php/useradd.php
@@ -0,0 +1,25 @@
+<?php
+    session_start();
+    header("Content-Type:text/html; charset=utf-8");
+    require_once 'config.php';
+
+    if (isset($_POST) && isset($_POST["username"]) && isset($_POST["password"]))
+    {
+        $id = $_POST['fullname'];
+        $un = $_POST['username'];
+        $pad = $_POST['password'];
+    } else {
+        die( 'Error: Username not found');
+    }
+
+    $sql = "INSERT INTO useracc (id, acc, pws) VALUES ('$id', '$un', '$pad')";
+
+    
+    if ($conn->query($sql) === TRUE) {
+        echo "<script>alert('New record created successfully');  location.href= '../../index.html' </script>";
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+        header('Location: ../../index.html');
+    }
+
+?>
