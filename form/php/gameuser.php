a7a671d2614c879ec6c6134a2c0d56afa3a91ec8
fenasot <79359861+fenasot@users.noreply.github.com>
2023-05-09 01:19:01 +0800

    fix bug

--- form/php/gameuser.php
+++ form/php/gameuser.php
@@ -1,8 +1,9 @@
 <?php
 
 session_start();
-header("Content-Type:text/html; charset=utf-8");
+header("Content-Type: text/plain charset=utf-8");
 require_once 'config.php';
+
 
 
 $un = $_SESSION['acc'];
