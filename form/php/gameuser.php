c09831a1e20351846dd594a0200fdaadaaa8359e
fenasot <79359861+fenasot@users.noreply.github.com>
2023-05-09 21:28:10 +0800

    up

--- form/php/gameuser.php
+++ form/php/gameuser.php
@@ -11,19 +11,12 @@ $sql = "SELECT * FROM useracc WHERE acc ='$un'";
 $result = mysqli_query($conn, $sql);
 $data = $result->fetch_assoc();
 
-/*
-$gamedata = array(
-    'userlocation' => $data["userlocation"],
-    'dice' => $data["dice"],
-);
-*/
 
 $gamedata = $data["userlocation"] . "a". $data["dice"];
 
-//$jsonData = json_encode($gamedata);
-
-header('Content-Type: application/json');
+ 
 
 echo $gamedata;
+
 exit();
 ?>
